e1c2fe31ba2352a7d5af528161a97631399fecae
Jesús González <jesus.gonzalez@simplepractice.com>
2026-07-12 13:23:02 -0600

    Introduce hammerspoon for macos multi stroke keymaps

---
+++ home/jesugq/.hammerspoon/init.lua
@@ -0,0 +1,26 @@
+local optTabTap = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
+    local keyCode = event:getKeyCode()
+    local flags = event:getFlags()
+
+    if keyCode == 48 then
+        if flags.cmd and not (flags.alt or flags.ctrl or flags.shift) then
+            hs.eventtap.keyStroke({"alt"}, "tab", 0)
+            return true
+        end
+    end
+
+    if keyCode == 33 then
+        if flags.ctrl then
+            hs.eventtap.keyStroke({}, "escape", 0)
+            return true
+        end
+    end
+
+    return false
+end)
+
+optTabTap:start()
+
+hs.alert.show("Hammerspoon Config Loaded: Opt+Tab remap active")
+hs.alert.show("Hammerspoon Config Loaded: Ctrl+[ remap active")
+
