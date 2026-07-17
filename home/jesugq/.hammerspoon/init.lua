7d80facd4c818720016c56cd88213ee5db9acf7e
jesugq <jesugq@outlook.com>
2026-07-16 18:25:42 -0600

    Add support for tab switching with Alt+[]

--- home/jesugq/.hammerspoon/init.lua
+++ home/jesugq/.hammerspoon/init.lua
@@ -16,11 +16,23 @@ multiTap = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
         end
     end
 
+    if keyCode == 33 then
+        if flags.alt and not (flags.ctrl or flags.cmd or flags.shift) then
+            hs.eventtap.keyStroke({"alt", "shift"}, "[", 0)
+            return true
+        end
+    end
+
+    if keyCode == 33 then
+        if flags.alt and not (flags.ctrl or flags.cmd or flags.shift) then
+            hs.eventtap.keyStroke({"alt", "shift"}, "[", 0)
+            return true
+        end
+    end
+
     return false
 end)
 
 multiTap:start()
-
-hs.alert.show("Hammerspoon Config Loaded: Opt+Tab remap active")
-hs.alert.show("Hammerspoon Config Loaded: Ctrl+[ remap active")
+hs.alert.show("Hammerspoon Config Loaded")
 
